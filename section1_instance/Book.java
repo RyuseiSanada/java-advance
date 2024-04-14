606caaa3b96566fc5c1ce353cf49cd74a8c48472
RyuseiSanada <sanadaryuusei@gmail.com>
2024-04-14 23:23:44 +0900

    section1

--- section1_instance/Book.java
+++ section1_instance/Book.java
@@ -1,7 +1,58 @@
-public class Book {
+import java.util.*;
+
+public class Book implements Comparable<Book>, Cloneable{
   private String title;
   private Date publishDate;
   private String comment;
+
+  public boolean equals(Object o) {
+    if (o == this) return true;
+    if (o == null) return false;
+    if (!(o instanceof Book)) return false;
+    Book b = (Book)o;
+    if(this.title == b.title && this.publishDate == b.publishDate) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  public int hashCode() {
+    return Objects.hash(this.title, this.publishDate, this.comment);
+  }
+
+  public int compareTo(Book b) {
+    return this.publishDate.compareTo(b.publishDate);
+  }
+
+  public Book clone() {
+    Book result = new Book();
+    setTitle(this.title);
+    // setPublishDate(this.publishDate);
+    setComment(this.comment);
+    return result;
+  }
   
   //getter, setter
+  public String getTitle() {
+    return this.title;
+  }
+  public Date getPublishDate() {
+    return this.publishDate;
+  }
+  public String getComment() {
+    return this.comment;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  public void setPublishDate(int year, int month, int date) {
+    this.publishDate = new Date(year, month, date);
+  }
+  public void setComment(String comment) {
+    this.comment = comment;
+  }
+
 }
