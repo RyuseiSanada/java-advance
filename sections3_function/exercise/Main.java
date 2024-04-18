117f9f33a4941b466510f2198f5c7232740f95ba
RyuseiSanada <sanadaryuusei@gmail.com>
2024-04-19 06:57:34 +0900

    らむだ

--- sections3_function/exercise/Main.java
+++ sections3_function/exercise/Main.java
@@ -4,25 +4,27 @@ public class Main{
   public static void main(String[] args) {
     FuncList funclist = new FuncList();
 
-    Func1 f1 = FuncList::isOdd;
-    Func2 f2 = funclist::passCheck;
+    Func1 f1 = x -> x % 2 == 1;
+    Func2 f2 = (point, name) -> String.format("%sさんは%s", name, point>65 ?"合格" : "不合格");
 
     System.out.println(f1.call(15));
     System.out.println(f2.call(66,"sanada"));
   }
 }
 
-class FuncList {
-  public static boolean isOdd(int x) {return (x %  2 == 1);}
-  public String passCheck(int point, String name) {
-    return String.format("%sさんは%s", name, point>65 ?"合格" : "不合格");
-  }
-}
+// class FuncList {
+//   public static boolean isOdd(int x) {return (x %  2 == 1);}
+//   public String passCheck(int point, String name) {
+//     return String.format("%sさんは%s", name, point>65 ?"合格" : "不合格");
+//   }
+// }
 
+@FunctionalInterface
 interface Func1 {
   boolean call(int x);
 }
 
+@FunctionalInterface
 interface Func2 {
   String call(int point, String name);
 }
