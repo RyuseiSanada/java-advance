17fbfed9b0891bac2c307bb9b9f4d59c55b33a4c
RyuseiSanada <sanadaryuusei@gmail.com>
2024-04-23 06:01:11 +0900

    プロパティーファイル

---
+++ section7_format/Main.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    
+    try(FileReader fr = new FileReader("pref.properties");)
+    {
+    Properties p = new Properties();
+
+    p.load(fr);
+    System.out.println(p.getProperty("aichi.capital"));
+    }
+    catch (IOException e) {
+      System.out.println("エラー");
+    }
+  }
+}
